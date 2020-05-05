33d24563551ea533e34df9c2785ae0a1ca708e5c
warrially <warrially@139.com>
2020-05-05 17:46:11 +0800

    new database

---
+++ gen/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/yangtizi/cz88"
+)
+
+func main() {
+	fmt.Println(cz88.GetAddress("47.56.187.111"))
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/yangtizi/cz88
 
 go 1.13
 
-require github.com/yangtizi/go v1.0.5
+require github.com/yangtizi/go v1.0.7
